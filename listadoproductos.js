92fe6579350048be9196767ec8b69aceef463b21
Gabriel Ferre Aracil <99750085+gafea84@users.noreply.github.com>
2023-01-11 10:57:26 +0100

    Add files via upload

---
+++ listadoproductos.js
@@ -0,0 +1,44 @@
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'Ifone 13',
+        precio: 899.99,
+        imagen: 'https://www.backmarket.es/cdn-cgi/image/format=auto,quality=75,width=640/https://d1eh9yux7w8iql.cloudfront.net/product_images/None_bb1e34e0-4112-46f4-b7e0-7394beba3f40.jpg'
+    },
+
+    {
+        id: 2,
+        nombre: 'Cable cargador',
+        precio: 19.99,
+        imagen: 'https://m.media-amazon.com/images/I/61XZIYZ94PL._AC_SX425_.jpg'
+    },
+
+    {
+        id: 3,
+        nombre: 'Base de carga',
+        precio: 49.99,
+        imagen: 'https://m.media-amazon.com/images/I/71f8mId9q6L._AC_SX425_.jpg'
+    },
+
+    {
+        id: 4,
+        nombre: 'Funda de silicona',
+        precio: 14.99,
+        imagen: 'https://images-na.ssl-images-amazon.com/images/I/51kxqWn7-EL.__AC_SX300_SY300_QL70_ML2_.jpg'
+    },
+
+    {
+        id: 5,
+        nombre: 'Funda de piel',
+        precio: 34.99,
+        imagen: 'https://www.estilux.com/22519-thickbox_default/funda-piel-mujjo-iphone-13-azul.jpg'
+    },
+
+    {
+        id: 6,
+        nombre: 'Protector cristal templado',
+        precio: 10.99,
+        imagen: 'https://m.media-amazon.com/images/I/61zUPTRRKnL._AC_SX425_.jpg'
+    }
+
+]
